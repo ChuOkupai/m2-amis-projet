6c673315484ae56db448f4233912d9ef8a9fce26
Coralie <coralie.zens@ens.uvsq.fr>
2023-01-04 20:32:58 +0100

    Datamining (chebi - download, hash and decompress)

--- srcs/python/datamining/chebi.py
+++ srcs/python/datamining/chebi.py
@@ -1,4 +1,13 @@
 from python.datamining.molecule import Molecule
+from python.common import constants
+
+from os import remove
+from os.path import exists
+import gzip
+import shutil
+import requests
+import hashlib
+
 
 class ChebiArchive:
 	"""Tools to download and extracts the ChEBI database."""
@@ -6,7 +15,9 @@ class ChebiArchive:
 	@staticmethod
 	def clear_cache():
 		"""Clears the cache. It removes the downloaded file."""
-		# TODO: Remove CHEBI_ARCHIVE_PATH.
+		# TODO: Test for Remove CHEBI_ARCHIVE_PATH.
+		if exists(constants.CHEBI_ARCHIVE_PATH):
+			remove(constants.CHEBI_ARCHIVE_PATH)
 
 	@staticmethod
 	def download(url):
@@ -16,9 +27,23 @@ class ChebiArchive:
 			url: The URL to the ChEBI database.
 
 		Raises:
-			IOError: If the file cannot be downloaded.
+			IOError: If the file cannot be downloaded. #TODO: Error can't be raised for test part
 		"""
-		# TODO: Download the file from url to CHEBI_ARCHIVE_PATH.
+		# TODO: Test for Download the file from url to CHEBI_ARCHIVE_PATH.
+		ChebiArchive.clear_cache()
+		try:
+			response = requests.get(url)
+		except Exception:
+			#print("Error in the chebi url or download : ",url)
+			raise Exception("IOError")
+		try:
+			f = open(constants.CHEBI_ARCHIVE_PATH, "wb")
+			f.write(response.content)
+			f.close()
+		except Exception:
+			#print("Error in coping file \n")
+			raise Exception("IOError")
+		
 
 	@staticmethod
 	def get_hash():
@@ -30,8 +55,24 @@ class ChebiArchive:
 		Raises:
 			IOError: If the file cannot be opened.
 		"""
-		# TODO: Return the SHA-256 of the file from CHEBI_ARCHIVE_PATH.
-		return 0x6f01af629fc8e15a3f88f38c698af68722298839214eada0590218699e0c4fbf
+		# TODO: Test for Return the SHA-256 of the file from CHEBI_ARCHIVE_PATH.
+		BUF_SIZE = 65536
+		sha256 = hashlib.sha256()
+		try :
+			with open(constants.CHEBI_ARCHIVE_PATH, 'rb') as f:
+				while True:
+					data = f.read(BUF_SIZE)
+					if not data:
+						f.close()
+						break
+					sha256.update(data)
+		except Exception:
+			raise Exception("IOError")
+		# TODO: move this code somewhere else
+		f = open(constants.CHEBI_HASH_PATH, "w")
+		f.write(sha256.hexdigest())
+		f.close()
+		return sha256.hexdigest() #0x6f01af629fc8e15a3f88f38c698af68722298839214eada0590218699e0c4fbf
 
 class ChebiDatabase:
 	"""Reads a ChEBI database as a structure-data file (SDF)."""
@@ -39,7 +80,9 @@ class ChebiDatabase:
 	@staticmethod
 	def clear_cache():
 		"""Clears the cache. It removes the extracted file."""
-		# TODO: Remove CHEBI_DATABASE_PATH.
+		# TODO: Test for Remove CHEBI_DATABASE_PATH.
+		if exists(constants.CHEBI_DATABASE_PATH):
+			remove(constants.CHEBI_DATABASE_PATH)
 
 	@staticmethod
 	def extract():
@@ -48,7 +91,25 @@ class ChebiDatabase:
 		Raises:
 			IOError: If the file cannot be extracted.
 		"""
-		# TODO: Extract the file from CHEBI_ARCHIVE_PATH to CHEBI_DATABASE_PATH.
+		# TODO: Test for Extract the file from CHEBI_ARCHIVE_PATH to CHEBI_DATABASE_PATH.
+		# TODO: Remove the downloaded file (did that mean the CHEBI_ARCHIVE one?)
+		f_in, f_out = None, None
+		try:
+			if not exists(constants.CHEBI_ARCHIVE_PATH):
+				raise Exception("FileNotFoundError")
+			with gzip.open(constants.CHEBI_ARCHIVE_PATH, 'rb') as f_in:
+				with open(constants.CHEBI_DATABASE_PATH, 'wb') as f_out:
+					shutil.copyfileobj(f_in, f_out)
+			f_in.close()
+			f_out.close()
+			#print("succesful decompress of the file ")
+		except Exception:
+			if f_in : 
+				f_in.close()
+			if f_out : 
+				f_out.close()
+			#print("Error in decompressing file \n")
+			raise Exception("IOError")
 
 	def __init__(self):
 		"""Open the ChEBI database file.
@@ -56,7 +117,13 @@ class ChebiDatabase:
 		Raises:
 			IOError: If the file cannot be opened.
 		"""
-		# TODO: Open the file from CHEBI_DATABASE_PATH and store the file object.
+		# TODO: Test for Open the file from CHEBI_DATABASE_PATH and store the file object
+		try :
+			self.file = open(constants.CHEBI_DATABASE_PATH, 'r')
+			self.file.close() # TODO: Move to the end process in next
+		except Exception:
+			self.file.close()
+			raise Exception("IOError")
 
 
 	def __iter__(self):
@@ -79,3 +146,4 @@ class ChebiDatabase:
 		"""
 		# TODO: Implement a parser for the SDF format.
 		return Molecule(0, "Molecule")
+
--- srcs/python/tests/test_datamining.py
+++ srcs/python/tests/test_datamining.py
@@ -1,9 +1,52 @@
 import unittest
-from python import datamining
+from python import datamining as dm
+from python.common import constants
+
+from os.path import exists
 
 class TestDatamining(unittest.TestCase):
 	"""This class contains the unit tests for the datamining module."""
 
-	def test_1_equals_1(self):
-		"""This test will always pass because 1 equals 1."""
+	def test_chebi_archive_clear(self):
+		"""This test is for ChebiArchive clear method"""
+		dm.ChebiArchive.clear_cache()
+		self.assertFalse(exists(constants.CHEBI_ARCHIVE_PATH))
+
+	def test_chebi_archive_download(self):
+		"""This test is for ChebiArchive download methodd"""
+		dm.ChebiArchive.download("https://ftp.ebi.ac.uk/pub/databases/chebi/SDF/ChEBI_lite_3star.sdf.gz")
+		# TODO: Change the test and the url dependance
+		self.assertTrue(exists(constants.CHEBI_ARCHIVE_PATH))
+		dm.ChebiArchive.clear_cache()
+
+	def test_chebi_archive_hash(self):
+		"""This test will test the hash."""
+		# TODO: Change the test
+		result = 1
+		expect = 0
+		if not exists(constants.CHEBI_ARCHIVE_PATH):
+			dm.ChebiArchive.download("https://ftp.ebi.ac.uk/pub/databases/chebi/SDF/ChEBI_lite_3star.sdf.gz")
+		result = dm.ChebiArchive.get_hash()
+		if exists(constants.CHEBI_HASH_PATH):
+			f = open(constants.CHEBI_HASH_PATH, "r")
+			expect = f.read()
+			f.close()
+		self.assertEqual(result, expect)
+
+	def test_chebi_database_clear(self):
+		"""This test is for ChebiDatabase clear method"""
+		dm.ChebiDatabase.clear_cache()
+		self.assertFalse(exists(constants.CHEBI_DATABASE_PATH))
+
+	def test_chebi_database_extract(self):
+		"""This test will extract the file from the archive."""
+		if not exists(constants.CHEBI_ARCHIVE_PATH):
+			dm.ChebiArchive.download("https://ftp.ebi.ac.uk/pub/databases/chebi/SDF/ChEBI_lite_3star.sdf.gz")
+		dm.ChebiDatabase.extract()
+		# TODO: Change the test and the url dependance
+		self.assertTrue(exists(constants.CHEBI_DATABASE_PATH))
+
+	def test_chebi_database_iterator(self):
+		"""This test will iter on molecule."""
+		# TODO: Create the test
 		self.assertEqual(1, 1)
